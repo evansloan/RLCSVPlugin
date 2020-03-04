24d81bff7e8e31b14cdada8e49950e573cb55435
Evan Sloan <evansloan082@gmail.com>
2020-03-03 19:05:31 -0500

    Remove unnecessary var

--- RLCSVPlugin/RLCSVPlugin.cpp
+++ RLCSVPlugin/RLCSVPlugin.cpp
@@ -2,14 +2,12 @@
 
 BAKKESMOD_PLUGIN(RLCSVPlugin, "RLCSV Plugin", "0.1", 0)
 
-const std::string saveLocation = "data/RLCSV";
-
 void RLCSVPlugin::onLoad() {
     std::stringstream ss;
     ss << exports.pluginName << " version: " << exports.pluginVersion;
     cvarManager->log(ss.str());
 
-    cvarManager->registerCvar("cl_rlcsv_csv_directory", "bakkesmod/" + saveLocation + "/", "Directory to write CSV files to (use forward slash '/' as separator in path", true, false, (0.0F), false, (0.0F), true);
+    cvarManager->registerCvar("cl_rlcsv_csv_directory", "bakkesmod/data/RLCSV/", "Directory to write CSV files to (use forward slash '/' as separator in path", true, false, (0.0F), false, (0.0F), true);
     cvarManager->getCvar("cl_rlcsv_csv_directory").addOnValueChanged(std::bind(&RLCSVPlugin::logCVarChange, this, std::placeholders::_1, std::placeholders::_2));
 
     gameWrapper->HookEvent("Function TAGame.GameEvent_Soccar_TA.OnMatchEnded", std::bind(&RLCSVPlugin::onMatchEnded, this, std::placeholders::_1));
